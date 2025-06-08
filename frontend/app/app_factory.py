5ed240fc9e871d7c35923b3fd3988f44cc300dcc
Daniil Dankovskyi <daniil@example.com>
2025-06-08 21:21:16 +0200

    codestyle

--- frontend/app/app_factory.py
+++ frontend/app/app_factory.py
@@ -9,14 +9,3 @@ def get_application() -> FastAPI:
     app.include_router(router)
 
     return app
-# from fastapi import FastAPI
-#
-# from settings import settings
-# from routers.router import router
-#
-# def get_application() -> FastAPI:
-#     app = FastAPI(root_path_in_servers=True, debug=settings.DEBUG)
-#     app.include_router(router)
-#     return app
-
-from fastapi.staticfiles import StaticFiles
