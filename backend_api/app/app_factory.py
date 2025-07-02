f11836de221d728918d6302bded64db07cc94f74
John Doe <johndoe@example.com>
2025-07-02 22:36:19 +0300

    Selected 3

--- backend_api/app/app_factory.py
+++ backend_api/app/app_factory.py
@@ -1,5 +1,5 @@
 from applications.auth.router import router_auth
-from applications.new_buildings.router import new_buildings_router
+from applications.new_buildings.router import new_buildings_router, selected_router
 from applications.users.router import router_users
 from fastapi import FastAPI
 from settings import settings
@@ -10,5 +10,6 @@ def get_application() -> FastAPI:
     app.include_router(router_users, prefix="/users", tags=["Users"])
     app.include_router(router_auth, prefix="/auth", tags=["Auth"])
     app.include_router(new_buildings_router, prefix="/new_buildings", tags=["NewBuildings"])
+    app.include_router(selected_router, prefix="/selected", tags=["Selected"])
 
     return app
